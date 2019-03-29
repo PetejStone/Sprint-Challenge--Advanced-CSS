0a82c1f2fd78b8374972250c795a1ff5c47f75da
PetejStone <petejstoney@gmail.com>
2019-03-29 12:25:11 -0700

    added sticky nav

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+const nav = document.getElementById('nav');
 
 /// carousel slideshow
 var slideIndex = 0;
@@ -15,3 +15,29 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+
+//sticky nav
+window.onscroll = function() {myFunction();};
+
+let sticky = nav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    nav.classList.add("sticky");
+    nav.style.maxWidth = "1200px";
+    nav.style.marginTop = 0;
+    nav.style.backgroundColor = "white";
+    nav.style.height = "3rem";
+    nav.style.borderRadius = '10px';
+    nav.style.opacity = '.7';
+
+
+  } else {
+    nav.classList.remove("sticky");
+    // intro.style.marginTop = "2em";
+    nav.style.marginTop = "1em";
+
+  }
+}
+//end of sticky nav
